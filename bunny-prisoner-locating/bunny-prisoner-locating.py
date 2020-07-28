97c5b28d68c3fa947113abb1dda94b28ce8bb3b4
shrey <shreykshah@gmail.com>
2020-07-28 15:03:44 -0700

    Bunny Prisoner Locating

---
+++ bunny-prisoner-locating/bunny-prisoner-locating.py
@@ -0,0 +1,7 @@
+def num(x,y):
+    #vertical = 1+((y(y-1))/2)
+    #horizontal = (x(x+1))/2
+    #adjustment factor = (x-1)(y-1) -1
+
+    #add and simplify
+    return str(((y*y+2*y*x-3*y+x*x-x)/2)+1)
